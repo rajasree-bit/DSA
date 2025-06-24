6e183928cd6a313fea431c2d24fe9d428f289010
rajasree-bit <chakrabortyrajasree23@gmail.com>
2025-06-24 18:40:10 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (52.95%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        int c=0,k=n;
-        if(n<=0){
+        if(n<=0)
+        return false;
+        
+        double b=log2(n);
+        int a=(int)b;
+        double diff=b-a;
+        if(diff!=0.0){
             return false;
         }
         else{
-        while(n!=1){
-            if(n%2==0){
-                c++;
-            }
-            else{
-                return false;
-            }
-            n=n/2;
-        }
-        if(pow(2,c)==k){
             return true;
         }
-        else{
-            return false;
-        }
-        }
-    } 
+}
 };
