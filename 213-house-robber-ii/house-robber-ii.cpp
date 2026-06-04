2b7cb1e4896c6ede63752c17bb8518b731069c5a
rajasree-bit <chakrabortyrajasree23@gmail.com>
2026-06-05 00:40:53 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (7.88%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -27,18 +27,13 @@ public:
     }
     int count(int l,vector<int>&nums,vector<int>&dp)
     {
-        if(l<=0)
-        return 0;
-        else if(l==1)
-            return nums[0];
-        else if(l==2)
-            return max(nums[0],nums[1]);
-        else
-            if(dp[l]!=-1)
-            {
-                return dp[l];
-            }
-            else
-            return dp[l]=max(nums[l-1]+count(l-2,nums,dp),count(l-1,nums,dp));
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+        for (int i=2;i<l;i++)
+        {
+            dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+        }
+        
+        return dp[l-1];
     }
 };
