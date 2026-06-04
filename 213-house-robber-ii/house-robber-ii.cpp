c65b3182c9fb6a0ee054ef0364487430f77f7230
rajasree-bit <chakrabortyrajasree23@gmail.com>
2026-06-05 01:16:29 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (86.89%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -2,38 +2,36 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int l=nums.size();
-        vector<int>num1(l-1,0);
-        vector<int>num2(l-1,0);
-        int a=0;
-        if(l==1)
-            return nums[0];
+        if(l<=0)
+        return 0;
+        else if(l==1)
+        return nums[0];
         else if(l==2)
-            return max(nums[0],nums[1]);
-        else{        
-            for (int i=0;i<l-1;i++)
-            {
-                num1[i]=nums[i];
-            }
-            for(int i=1;i<l;i++)
-            {
-                num2[a]=nums[i];a++;
-            }
-            vector<int>dp(l,-1);
-            int n1=count(l-1,num1,dp);
-            vector <int>dp1(l,-1);
-            int n2=count(l-1,num2,dp1);
-            return max(n1,n2);
-        }
-    }
-    int count(int l,vector<int>&nums,vector<int>&dp)
-    {
-        dp[0]=nums[0];
-        dp[1]=max(nums[0],nums[1]);
-        for (int i=2;i<l;i++)
+        return max(nums[0],nums[1]);
+        else
         {
-            dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+        int prev1=0,prev2=0,curr=0;
+        prev1=nums[0];
+        prev2=max(nums[0],nums[1]);
+        curr=prev2;
+        for(int i=2;i<l-1;i++)
+        {
+            curr=max(nums[i]+prev1,prev2);
+            prev1=prev2;
+            prev2=curr;
         }
-        
-        return dp[l-1];
+        int curr1=0;
+        prev1=nums[1];
+        prev2=max(nums[1],nums[2]);
+
+        curr1=prev2;
+        for(int i=3;i<l;i++)
+        {
+            curr1=max(nums[i]+prev1,prev2);
+            prev1=prev2;
+            prev2=curr1;
+        }
+        return max(curr,curr1);
+        }
     }
 };
